421368ca3af5d90b2c7f89e633ee3e29ff34903f
redxtreme <zahopper@mtu.edu>
2018-01-03 19:59:39 -0600

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Coding-Train-Practice
+
+## Test Locally
+To test locally, navigate to directory in terminal and type [code]python -m http.server[/code]
---
+++ astar/sketch.js
@@ -0,0 +1,14 @@
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+
+function setup() {
+  createCanvas(400, 400);
+  console.log('A*');
+
+}
+
+function draw() {
+  background(0);
+
+}
