a6efccaec5fa6f1f79629fbb30f7e9f64563efda
redxtreme <zahopper@mtu.edu>
2018-01-07 16:09:09 -0600

    Project setup

---
+++ traveling-salesperson/sketch.js
@@ -0,0 +1,13 @@
+var cities = [];
+
+// Setup function required by p5
+function setup() {
+  createCanvas(400, 400);
+  console.log('Project Started');
+
+}
+
+// Animation loop
+function draw() {
+  background(51);
+}
