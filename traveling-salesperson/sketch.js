1dc62aede65265df354aba6c5119570f0c6dd4de
redxtreme <zahopper@mtu.edu>
2018-01-07 16:26:50 -0600

    Paths drawn

--- traveling-salesperson/sketch.js
+++ traveling-salesperson/sketch.js
@@ -6,6 +6,7 @@ function setup() {
   createCanvas(400, 400);
   console.log('Project Started');
 
+  // Create cities
   for (var i = 0; i < totalCities; i++) {
     var v = createVector(random(width), random(height));
     cities[i] = v;
@@ -16,7 +17,23 @@ function setup() {
 function draw() {
   background(51);
   fill(255);
+
+  // Draw the cities
   for (var i = 0; i < cities.length; i++) {
-    ellipse(cities[i].x, cities[i].y, 4, 4);
+    ellipse(cities[i].x, cities[i].y, 8, 8);
   }
+
+  beginShape();
+  noFill();
+  strokeWeight(2);
+  stroke(255);
+  // Draw the paths
+  for (var i = 0; i < cities.length; i++) {
+    vertex(cities[i].x, cities[i].y);
+  }
+  endShape();
+}
+
+function swap(a, i, j)) {
+
 }
