4afc0a6c4f7edca4108e938fcb5f156ecfbca073
redxtreme <zahopper@mtu.edu>
2018-01-18 22:19:29 -0600

    Poison drawn

--- evolutionary-steering/sketch.js
+++ evolutionary-steering/sketch.js
@@ -1,13 +1,23 @@
 var vehicle
 var food = [];
+var poison = [];
 
 function setup() {
   createCanvas(640, 360);
   vehicle = new Vehicle(width / 2, height / 2);
+
+  // Populate poison
   for (var i = 0; i < 10; i++) {
     var x = random(width);
     var y = random(height);
     food.push(createVector(x, y));
+  }
+
+  // Populate poison
+  for (var i = 0; i < 10; i++) {
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
   }
 }
 
@@ -21,14 +31,23 @@ function draw() {
   // strokeWeight(2);
   // ellipse(target.x, target.y, 48, 48);
 
+  // Draw food
   for (var i = 0; i < food.length; i++) {
     fill(255, 0, 255);
     noStroke();
     ellipse(food[i].x, food[i].y, 8, 8);
   }
 
+  // Draw poison
+  for (var i = 0; i < poison.length; i++) {
+    fill(0, 150, 0);
+    noStroke();
+    ellipse(poison[i].x, poison[i].y, 8, 8);
+  }
+
   //vehicle.seek(target);
   vehicle.eat(food);
+  vehicle.eat(poison);
   vehicle.update();
   vehicle.display();
 }
