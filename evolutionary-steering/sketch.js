2297497dda2621966879e97a8811d20705af2405
redxtreme <zahopper@mtu.edu>
2018-01-21 12:48:46 -0600

    New food generates

--- evolutionary-steering/sketch.js
+++ evolutionary-steering/sketch.js
@@ -30,12 +30,12 @@ function setup() {
 function draw() {
   background(51);
 
-  // var target = createVector(mouseX, mouseY);
-
-  // fill(127);
-  // stroke(200);
-  // strokeWeight(2);
-  // ellipse(target.x, target.y, 48, 48);
+  // Add new food randomly
+  if (random(1) < 0.05) {
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
+  }
 
   // Draw food
   for (var i = 0; i < food.length; i++) {
